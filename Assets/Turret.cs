45427406b48e5fc049b16c682189f7d594a9eb59
Geoff Hill @ SAE Institute <g.hill@sae.edu>
2024-11-12 17:40:19 +0000

    added vfx

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -10,6 +10,17 @@ public class Turret : MonoBehaviour
     public Vector3 pointA;
     public Vector3 pointB;
     public Color lineColor = Color.red;
+    public float launchWait = 1.0f;
+
+    public enum TurretSM
+    {
+        Following, Launching, Firing
+
+    }
+    public TurretSM turretSM;
+
+    public GameObject bulletBillPrefab;
+    public Transform spawnPointPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +31,44 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Transform closestEnemytransform = enemyManager.ClosestEnemy().transform;
-        transform.LookAt(closestEnemytransform.position);
 
-        pointA = transform.position;
-        pointB = closestEnemytransform.position;
+        switch (turretSM)
+        {
+            case TurretSM.Following:
+                Transform closestEnemytransform = enemyManager.ClosestEnemy().transform;
+                transform.LookAt(closestEnemytransform.position);
+
+                pointA = transform.position;
+                pointB = closestEnemytransform.position;
+
+
+                //count down launchWait time looking at the point
+                launchWait -= Time.deltaTime;
+                if (launchWait < 0)
+                {
+                    turretSM = TurretSM.Firing;
+                    launchWait = 1;
+                }
+                break;
+
+            case TurretSM.Launching:
+                
+
+                break;
+
+            case TurretSM.Firing:
+
+                //fire projectile
+                Instantiate(bulletBillPrefab, spawnPointPosition.position, transform.rotation);
+
+
+                turretSM = TurretSM.Following;
+
+            break;
+
+        }
+
+
     }
 
 
